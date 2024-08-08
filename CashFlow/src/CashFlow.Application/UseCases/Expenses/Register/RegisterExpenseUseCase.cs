c7c27aec9c581026b498441fc8786946835f19a7
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-08 13:20:04 -0300

    feat: Created the structure for the expensive business rule

---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public RequestRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            // TO DO VALIDATIONS
+
+            return new RequestRegisterExpenseJson();
+        }
+    }
+}
